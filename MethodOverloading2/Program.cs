6f02542912951a50a625838be14a635f800eb384
David Smith <cpmpdavidsmith@icloud.com>
2023-02-09 09:38:13 -0500

    mmethod overloading 2

---
+++ MethodOverloading2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MethodOverloading
+{
+    public class Program
+    {
+        //-------------------------TEST AREA-------------------------------------
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5, 2));
+            //Console.WriteLine(Add( 4.35, 2.65));
+            //Console.WriteLine(Add(5, -4, true));
+        }
+        //----------------------METHOD OVERLOAD----------------------------------
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool currency)
+        {
+            int total = x + y;
+
+            if ((total < 1 || total > 1) && true)
+            {
+                return $"{total} dollars";
+            }
+            else 
+            {
+                return $"{total} dollar";
+            }
+           
+        }
+
+    }
+}
